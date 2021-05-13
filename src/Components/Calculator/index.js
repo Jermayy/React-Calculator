8ece61d60c7cd5607f2fb594fdb3658f7dadfefb
Jeremy <jeremyVDLRC@gmail.com>
2021-05-13 15:09:21 +1000

    grid layout, and result box place holder

--- src/Components/Calculator/index.js
+++ src/Components/Calculator/index.js
@@ -9,16 +9,53 @@ const Calculator = (): React.Node => {
 const Styles = {
   container:{
     border: 'solid',
-    backgroundColor: 'grey',
+    backgroundColor: 'red',
     width: '300px',
-    height: '500px'
+    height: '500px',
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  grid:{
+      display: 'grid',
+            gridTemplateColumns: 'auto auto auto',
+            gridTemplateRows: 'auto auto auto',
+            gap: '10px',
+            justifyContent: 'center'
+        
+            
+  },
+  resultBox:{ 
+      border: 'solid',
+      height: '40px',
+      width: '90%',
+      backgroundColor: 'White',
+       justifyContent: 'center'
+
   }
 }
 return (
-     <div style={Styles.container}>
-        Calculator
+
+    <div style={Styles.container}> 
+    Calculator
+
+<div style={Styles.resultBox}>
+Placeholder result box
+</div>
+
+    <div style={Styles.grid}>
         <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        <Button/>
+        
     </div>
+    </div>
+     
 )
 
 }
